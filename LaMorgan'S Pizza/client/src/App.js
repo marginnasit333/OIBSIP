e2f7c5f972833fbbd9b7b7dfce7367430ada7883
marginnasit333 <marginnasit666@gmail.com>
2023-05-08 09:42:34 +0530

    Start

---
+++ LaMorgan'S Pizza/client/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import { Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <>
+      <Container style={{ marginTop: "200px" }}>
+        <h4 className="text-center text-dark bg-warning p-2 mt-4"> Welcome To</h4>
+        <h1 className="text-center text-white bg-dark p-2 mt-4">
+          LaMorgan"S Pizza
+        </h1>
+        <h2 className="m-4 text-center">
+          follow on Instagram :
+          <strong style={{ color: "blue" }}> @LaMorgan"s_Pizza</strong>
+        </h2>
+        <h1>
+         
+        </h1>
+      </Container>
+    </>
+  );
+}
+
+export default App;
